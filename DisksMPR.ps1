39d13d220039d86498db177cb3f5dd9de52d0354
bobrovkir <bobrovkir@gmail.com>
2023-05-03 10:18:47 +0300

    1

---
+++ DisksMPR.ps1
@@ -0,0 +1,11 @@
+﻿$TTNumber=read-host "введите номер магазина - прим. 241"
+$TTPassword=read-host "введите пароль"
+[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
+
+#Подключение диска Документы для магазинов МПР
+net use Z: "\\ad.modniy.org\change_r2\Документы для магазинов МПР" /user:modniy\magazin$TTNumber /persistent:yes
+net use Y: "\\ad.modniy.org\change_r2\Change\Magazins\$TTNumber"   /user:modniy\magazin$TTNumber /persistent:yes
+
+#Команда на отключение сетевого диска: net use Z: /delete, всех дисков: net use * /delete
+
+ 
